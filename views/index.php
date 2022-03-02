297f1ee72d1e3dbe6388994a83b865fe64b7dfd8
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2022-03-02 10:12:26 -0400

    update code repo php start

---
+++ views/index.php
@@ -0,0 +1,9 @@
+<?php
+$Component = new Component();
+echo $Component->getHeadHtml();
+?>
+<div class="container">
+    <h4>Curso de PHP</h4>
+    <span>PHP start</span>
+</div>
+<?= $Component->getFooter() ?>
